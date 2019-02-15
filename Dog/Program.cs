4d0119b0e7beef0f4a71ed5d127c10b8a532b2cd
Andrew Schuster <hello@Andys-Mac.local>
2019-02-15 12:07:02 -0600

    First Commit

---
+++ Dog/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace DogChallenge
+{
+    public class Dog
+    {
+        public enum Gender
+        {
+            Male,
+            Female
+        }
+        public String name;
+        public String owner;
+        public int age;
+        public Gender gender;
+
+        /*Switch Case for Gender*/
+        public static string GetGender(Gender gender)
+        {
+            if (gender == Gender.Male)
+            {
+                return "His";
+            }
+            else
+            {
+                return "Her";
+            }
+        }
+        //Dog Method
+        public Dog(String pet1, String owner1, int dog1, Gender dogGender)
+        {
+            name = pet1;
+            owner = owner1;
+            age = dog1;
+            gender = dogGender;
+        }
+
+        //Bark Method
+        public void Bark(int num)
+        {
+            for (int i = 0; i < num; i++)
+            {
+                Console.WriteLine("Woof!");
+            }
+        }
+        public string GetTag()
+        {
+            string yearorYears;
+            if (age == 1)
+            {
+                yearorYears = " year ";
+            }
+            else
+            {
+                yearorYears = " years ";
+            }
+            string hisorher = GetGender(gender).ToString();
+            string heorshe;
+            if (hisorher == "His")
+            {
+                heorshe = "he";
+            }
+            else
+            {
+                heorshe = "she";
+            }
+            return "If lost, call " + owner + " " + hisorher + " name is " + name + " and " + heorshe + " is " + age + yearorYears + "old.";
+        }
+
+        static void Main(string[] args)
+        {
+            Dog puppy = new Dog("Orion", "Shawn", 1, Gender.Male);
+            puppy.Bark(3);
+            Console.WriteLine(puppy.GetTag());
+
+            Dog myDog = new Dog("Lileu", "Dale", 4, Gender.Female);
+            myDog.Bark(1);
+            Console.WriteLine(myDog.GetTag());
+        }
+    }
+}
